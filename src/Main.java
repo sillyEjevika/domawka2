e11e4fe1bb2d193daa7713a5c09f15eb424db516
sillyEjevika <Doroteia_Vika@mail.ru>
2022-12-03 21:57:27 +0300

    Задание №2 (задача 4-5)

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+      // Задание 4
+        System.out.println("Задание №4");
+       var friend = 19;
+        System.out.println(friend);
+        friend = friend + 2;
+        System.out.println(friend);
+        friend = friend / 2;
+        System.out.println(friend);
+        // Задание 5
+        System.out.println("Задание №5");
+        var frog = 3.5;
+        System.out.println (frog);
+        frog = frog * 10;
+        System.out.println (frog);
+        frog = frog / 3.5;
+        System.out.println (frog);
+        frog = frog + 4;
+        System.out.println (frog);
+    }
+}
